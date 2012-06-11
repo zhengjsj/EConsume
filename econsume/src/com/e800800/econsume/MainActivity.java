b51a66189db0fd5b186c9b962fb92e7d643d8f71
mattxian <xsuper123>
2012-06-11 21:43:48 +0800

    create project

---
+++ econsume/src/com/e800800/econsume/MainActivity.java
@@ -0,0 +1,13 @@
+package com.e800800.econsume;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
